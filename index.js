8a95006b1a921f90817440a6f1d801cb8e9f2baf
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-06-03 15:12:32 -0400

    All Tests Pass

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace(/[^a-zA-Z0-9-' ]/g, '')
+  }
+
+  static titleize(string){
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    const words = string.split(' ')
+    const titleizedString = words.map((word, index) => {
+      if(index === 0 || !exceptions.includes(word)){
+        return word.charAt(0).toUpperCase() + word.slice(1)
+      }else {
+        return word
+      }
+    })
+    return titleizedString.join(' ')
+  }
 }
